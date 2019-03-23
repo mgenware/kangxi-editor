770d7c0174235aae19cf00c49e174864a21d2328
Mgen <m@mgenware.com>
2019-03-23 17:11:28 +0800

    Wrap prosemirror editor in a dedicated class

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { baseKeymap } from 'prosemirror-commands';
 import { keymap } from 'prosemirror-keymap';
 import { EditorState, Transaction } from 'prosemirror-state';
-import { DOMParser } from 'prosemirror-model';
+import { DOMParser, Schema } from 'prosemirror-model';
 import { schema } from './schema/schema';
 import { EditorView } from 'prosemirror-view';
 import { history } from 'prosemirror-history';
@@ -10,14 +10,33 @@ import { throwIfEmpty } from 'throw-if-arg-empty';
 import './style.css';
 import { buildKeymap } from './keys/keymap';
 
-export function mount(selector: string) {
+export class Editor {
+  constructor(
+    public core: EditorView,
+    public editorSchema: Schema,
+    public contentElement: HTMLElement,
+  ) {}
+
+  htmlContent(): string {
+    const root = this.contentElement;
+    if (root.firstElementChild) {
+      return root.firstElementChild.innerHTML;
+    }
+    return '';
+  }
+}
+
+export function mount(selector: string, content?: string): Editor {
   throwIfEmpty(selector, 'selector');
+  content = content || '';
   const editorElement = document.querySelector(selector);
   if (!editorElement) {
     throw new Error(`The selector "${selector}" does not match anything`);
   }
   const toolbarElement = editorElement.querySelector('.kx-toolbar');
-  const contentElement = editorElement.querySelector('.kx-content');
+  const contentElement = editorElement.querySelector(
+    '.kx-content',
+  ) as HTMLElement;
 
   if (!toolbarElement) {
     throw new Error(`ToolBar element not found inside "${selector}"`);
@@ -26,24 +45,24 @@ export function mount(selector: string) {
     throw new Error(`Content element not found inside "${selector}"`);
   }
 
-  if (editorElement) {
-    const state = EditorState.create({
-      doc: DOMParser.fromSchema(schema).parse(document.querySelector(
-        '#content',
-      ) as HTMLElement),
-      plugins: [
-        history(),
-        keymap(buildKeymap(schema, null)),
-        keymap(baseKeymap),
-        setupToolbar(toolbarElement as HTMLElement),
-      ],
-    });
-    const view = new EditorView(editorElement, {
-      state,
-      dispatchTransaction(transaction: Transaction) {
-        const newState = view.state.apply(transaction);
-        view.updateState(newState);
-      },
-    });
-  }
+  const srcElement = document.createElement('div') as HTMLElement;
+  srcElement.innerHTML = content;
+
+  const state = EditorState.create({
+    doc: DOMParser.fromSchema(schema).parse(srcElement),
+    plugins: [
+      history(),
+      keymap(buildKeymap(schema, null)),
+      keymap(baseKeymap),
+      setupToolbar(toolbarElement as HTMLElement),
+    ],
+  });
+  const view = new EditorView(contentElement, {
+    state,
+    dispatchTransaction(transaction: Transaction) {
+      const newState = view.state.apply(transaction);
+      view.updateState(newState);
+    },
+  });
+  return new Editor(view, schema, contentElement);
 }
